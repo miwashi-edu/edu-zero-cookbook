13716589c95ab9b1e0fdf0d6e80d3d631c620c49
Mikael Wallin <mikael.x.wallin@gmail.com>
2025-04-14 02:55:40 +0000

    Initial commit

---
+++ build/src/CMakeFiles/hello1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/dev/ws/zero-project/bin/hello1"
+  "/home/dev/ws/zero-project/bin/hello1.pdb"
+  "CMakeFiles/hello1.dir/main.cpp.o"
+  "CMakeFiles/hello1.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/hello2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/dev/ws/zero-project/bin/hello2"
+  "/home/dev/ws/zero-project/bin/hello2.pdb"
+  "CMakeFiles/hello2.dir/main.cpp.o"
+  "CMakeFiles/hello2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
